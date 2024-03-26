dab3ac3c5192bcd582aae1e865ebd537f568d061
SushanShakya <sushaanshakya88@gmail.com>
2024-03-26 11:04:11 +0545

    Feat: Initialization

---
+++ bin/apk_automation_dart.dart
@@ -0,0 +1,5 @@
+import 'package:apk_automation_dart/apk_automation_dart.dart' as apk_automation_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${apk_automation_dart.calculate()}!');
+}
